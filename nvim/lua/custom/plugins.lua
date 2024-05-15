190610ce538567bdb200b93ab2e8e2ce8f2cae35
SiddharthMaheshB <125998948+SiddharthMaheshB@users.noreply.github.com>
2024-05-15 23:17:15 +0530

    initial commit :nerd:

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,18 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+return plugins
